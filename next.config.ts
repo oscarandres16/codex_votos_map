3f7c7ea3c92193eba6f2824585b81115381b855b
Oscar Andres <rsi.dev9@uis.edu.co>
2026-03-12 17:13:56 -0500

    feat: :rocket: trying to deploy using gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+const repo = "votantes-map"; // nombre del repo
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
